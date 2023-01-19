2371c919588f1ce4f04700c7b2d6b79f3655421b
Evan McLean <evanhughmclean@gmail.com>
2023-01-19 12:38:16 -0500

    fix: Change file structure and add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
-import Header from './components/Header';
-import Main from './components/Main'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Game from "./components/Game";
+import Header from "./components/Header";
+import Leaderboard from "./components/Leaderboard";
+import StartGame from "./components/StartGame";
 
 function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartGame />}/>
+          <Route path="/game" element={<Game />}/>
+          <Route path="/leaderboard" element={<Leaderboard />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
