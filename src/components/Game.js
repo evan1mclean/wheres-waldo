6d15dacc767656295ba58640c79ad5a26f0ce932
Evan McLean <evanhughmclean@gmail.com>
2023-01-26 16:12:07 -0500

    feat: Add state for target box and add component to game contaner

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,9 +5,16 @@ import waldo from "../images/waldoHeader.png";
 import gameImage from "../images/game-image.jpg";
 import "../styles/Game.css";
 import { useEffect, useState } from "react";
+import TargetBox from "./TargetBox";
 
 export default function Game() {
+  //state for the timer
   const [timer, setTimer] = useState(0);
+   //state for targeting box
+   const [isVisible, setIsVisible] = useState(false);
+   //state for target box size/coordinates
+   const [pageX, setPageX] = useState(0);
+   const [pageY, setPageY] = useState(0);
 
   //function for formatting the time correctly
   function formatTime(time) {
@@ -15,19 +22,38 @@ export default function Game() {
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = time % 60;
     if (hours >= 1) {
-      return `${hours.toString().padStart(2, '0')}:${minutes
+      return `${hours.toString().padStart(2, "0")}:${minutes
         .toString()
-        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
     } else {
-      return `${minutes.toString().padStart(2, '0')}:${seconds
+      return `${minutes.toString().padStart(2, "0")}:${seconds
         .toString()
-        .padStart(2, '0')}`;
+        .padStart(2, "0")}`;
+    }
+  }
+
+  function getClickPosition(e) {
+    const x = Math.floor((100 * e.nativeEvent.offsetX) / e.target.offsetWidth);
+    const y = Math.floor((100 * e.nativeEvent.offsetY) / e.target.offsetHeight);
+    return { x, y };
+  }
+
+
+  function handleClick(e) {
+    if (!isVisible) {
+      const { x, y } = getClickPosition(e);
+      console.log(e)
+      setPageX(e.pageX);
+      setPageY(e.pageY);
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
     }
   }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTimer(prevTimer => prevTimer + 1);
+      setTimer((prevTimer) => prevTimer + 1);
     }, 1000);
     return () => clearInterval(interval);
   }, []);
@@ -55,7 +81,12 @@ export default function Game() {
       </div>
 
       <div className="game-main">
-        <img src={gameImage} alt="Universe 113 Where's Waldo Type Game" />
+        <img
+          src={gameImage}
+          alt="Universe 113 Where's Waldo Type Game"
+          onClick={handleClick}
+        />
+        <TargetBox isVisible={isVisible} x={pageX} y={pageY}/>
       </div>
     </div>
   );
